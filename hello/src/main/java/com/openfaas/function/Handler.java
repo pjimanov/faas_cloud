f621b3f30a536aec644815f3538783397ba45eb7
Pav Jimanov <pav@softwaredefined.io>
2018-10-29 21:06:35 -0600

    update time

--- hello/src/main/java/com/openfaas/function/Handler.java
+++ hello/src/main/java/com/openfaas/function/Handler.java
@@ -4,12 +4,18 @@ import com.openfaas.model.IHandler;
 import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world...");
+        SimpleDateFormat dateTimeInGMT = new SimpleDateFormat("yyyy-MMM-dd hh:mm:ss aa");
+	    dateTimeInGMT.setTimeZone(TimeZone.getTimeZone("GMT"));
+        res.setBody("Hello, Pav...");
+        res.setBody(dateTimeInGMT.format(new Date()));
 	    return res;
     }
 }
