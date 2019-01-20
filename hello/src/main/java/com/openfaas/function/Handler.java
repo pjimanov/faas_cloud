e03dd431e921c92153462f1cc7e31fae3f7784cc
Pav Jimanov <pav@softwaredefined.io>
2019-01-20 13:15:43 -0700

    Update Handler.java

--- hello/src/main/java/com/openfaas/function/Handler.java
+++ hello/src/main/java/com/openfaas/function/Handler.java
@@ -15,7 +15,7 @@ public class Handler implements com.openfaas.model.IHandler {
         SimpleDateFormat dateTimeInGMT = new SimpleDateFormat("yyyy-MMM-dd hh:mm:ss aa");
 	dateTimeInGMT.setTimeZone(TimeZone.getTimeZone("GMT"));
         //res.setBody(dateTimeInGMT.format(new Date()));
-	res.setBody("HI MICHAEL");
+	res.setBody("i am 6 and i am writing code");
 	return res;
     }
 }
